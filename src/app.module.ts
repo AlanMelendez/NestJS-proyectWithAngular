2a4809b96bfcd3c7401dbc741c2aae8b58d74a11
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2024-05-10 17:39:01 -0600

    C:/Program Files/Git/Add Config module for view enviroment variables in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    // ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env); //Ver todas las variables de entorno corriendo.
+  }
+}
