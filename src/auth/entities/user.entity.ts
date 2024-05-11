f373d18dcc2b6004511c9bcb208dad62515b59d7
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2024-05-10 18:05:11 -0600

    C:/Program Files/Git/Add Generate (Shema/Entity) User

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+
+
+@Schema()
+export class User {
+
+    @Prop({ required: true, unique: true})
+    email: string;
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ minlength: 6, required: true})
+    password: string;
+
+    @Prop({ default: true })
+    isActive: boolean;
+
+    @Prop({ type: [String], default: ['user'] })
+    roles: string[];
+
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User); //Esto es para que mongoose(back) sepa que es un schema de mongoose.
