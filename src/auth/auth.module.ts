eefe07629bf9efd4fe60eb8a4dc03d15d19a48d8
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2024-05-10 18:09:20 -0600

    C:/Program Files/Git/Add Generate config in authModule for UserSchema works in mongoose and expose schema.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    MongooseModule.forFeature([
+      //Expose any mongoose schemas here
+      {
+        name: User.name, //The name of the model
+        schema: UserSchema //The schema to be used
+        // schema: {
+        //   email: { type: String, required: true, unique: true },
+        //   name: { type: String, required: true },
+        //   password: { type: String, required: true, minlength: 6 },
+        //   isActive: { type: Boolean, default: true },
+        //   roles: { type: [String], default: ['user'] },
+        // },
+      }
+    ]),
+  ]
 })
 export class AuthModule {}
