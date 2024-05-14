e48b541adda7ae49121afacaca12e3f8a7bd3a56
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2024-05-13 20:04:21 -0600

    Add Config with JWT in root module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,25 +3,30 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+
+    ConfigModule.forRoot(),
+
     MongooseModule.forFeature([
       //Expose any mongoose schemas here
       {
         name: User.name, //The name of the model
         schema: UserSchema //The schema to be used
-        // schema: {
-        //   email: { type: String, required: true, unique: true },
-        //   name: { type: String, required: true },
-        //   password: { type: String, required: true, minlength: 6 },
-        //   isActive: { type: Boolean, default: true },
-        //   roles: { type: [String], default: ['user'] },
-        // },
       }
     ]),
+
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+
+    }),
   ]
 })
 export class AuthModule {}
